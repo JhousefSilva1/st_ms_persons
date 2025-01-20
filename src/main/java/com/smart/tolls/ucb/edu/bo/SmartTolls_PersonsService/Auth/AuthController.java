f317647e7304b6e06a6f7eaae3ec2084123b48d4
JhousefSilva1 <jhosede23@gmail.com>
2025-01-20 11:50:28 -0400

    adding apiResponse AuthController

--- src/main/java/com/smart/tolls/ucb/edu/bo/SmartTolls_PersonsService/Auth/AuthController.java
+++ src/main/java/com/smart/tolls/ucb/edu/bo/SmartTolls_PersonsService/Auth/AuthController.java
@@ -1,32 +1,74 @@
 package com.smart.tolls.ucb.edu.bo.SmartTolls_PersonsService.Auth;
 
+import com.smart.tolls.ucb.edu.bo.SmartTolls_PersonsService.Controller.ApiController;
+import com.smart.tolls.ucb.edu.bo.SmartTolls_PersonsService.Models.Response.ApiResponse;
+import jakarta.validation.ConstraintViolationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
-public class AuthController {
+public class AuthController extends ApiController {
 
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<TokenResponse> register(@RequestBody RegisterRequest request){
-        final TokenResponse response = authService.register(request);
-        return ResponseEntity.ok(response);
+    public ApiResponse<TokenResponse> register(@RequestBody RegisterRequest request){
+        ApiResponse<TokenResponse> response = new ApiResponse<>();
+        try {
+            TokenResponse token = authService.register(request);
+            response.setData(token);
+            response.setStatus(HttpStatus.OK.value());
+            response.setMessage(HttpStatus.OK.getReasonPhrase());
+        } catch (ConstraintViolationException e) {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            response.setMessage(HttpStatus.BAD_REQUEST.getReasonPhrase());
+        } catch (Exception e) {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            response.setMessage(HttpStatus.BAD_REQUEST.getReasonPhrase());
+        }
+        return logApiResponse(response);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<TokenResponse> authenticate(@RequestBody AuthRequest request){
-        final TokenResponse response = authService.authenticate(request);
-        return ResponseEntity.ok(response);
+    public ApiResponse<TokenResponse> authenticate(@RequestBody AuthRequest request){
+        ApiResponse<TokenResponse> response = new ApiResponse<>();
+        try {
+            TokenResponse token = authService.authenticate(request);
+            response.setData(token);
+            response.setStatus(HttpStatus.OK.value());
+            response.setMessage(HttpStatus.OK.getReasonPhrase());
+        } catch (ConstraintViolationException e) {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            response.setMessage(HttpStatus.BAD_REQUEST.getReasonPhrase());
+        } catch (Exception e) {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            response.setMessage(HttpStatus.BAD_REQUEST.getReasonPhrase());
+        }
+        return logApiResponse(response);
     }
 
     @PostMapping("/refresh-token")
-    public TokenResponse refreshToken(@RequestHeader(HttpHeaders.AUTHORIZATION) final String authentication){
-        return authService.refreshToken(authentication);
-
+    public ApiResponse<TokenResponse> refreshToken(@RequestHeader(HttpHeaders.AUTHORIZATION) final String authentication){
+        ApiResponse<TokenResponse> response = new ApiResponse<>();
+        try {
+            TokenResponse token = authService.refreshToken(authentication);
+            response.setData(token);
+            response.setStatus(HttpStatus.OK.value());
+            response.setMessage(HttpStatus.OK.getReasonPhrase());
+        } catch (ConstraintViolationException e) {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            response.setMessage(HttpStatus.BAD_REQUEST.getReasonPhrase());
+        } catch (Exception e) {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            response.setMessage(HttpStatus.BAD_REQUEST.getReasonPhrase());
+        }
+        return logApiResponse(response);
     }
 }
